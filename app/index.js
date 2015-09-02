3b44bcf6a83b5a58834e24447bd22a9008dd6d58
Jason Stallings <jason@stallin.gs>
2015-09-02 04:04:21 -0500

    Basic generator.

---
+++ app/index.js
@@ -0,0 +1,48 @@
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+var easynwjsgenerator = yeoman.generators.Base.extend(
+{
+    initializing: function () 
+    {
+        this.pkg = require('../package.json');
+    },
+
+    prompting: function () 
+    {
+        var done = this.async();
+
+        var prompts = [
+            {
+                name: 'appName',
+                message: 'What is your apps\'s name?',
+                default: 'My App'
+            }
+        ];
+
+        this.prompt(prompts, function (props) 
+        {
+            this.appName = props.appName;
+            done();
+        }.bind(this));
+    },
+
+
+    files: function () 
+    {
+        var context = 
+        {
+            app_name: this.appName
+        };
+        this.template('www/_index.html', 'index.html', context);
+        this.template('www/_package.json', 'package.json', context);
+    },
+
+    end: function () 
+    {
+
+    }
+});
+
+module.exports = easynwjsgenerator;
